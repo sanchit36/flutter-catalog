4042093af9c8551cc22d7d575a394580b5e86ef3
Sanchit <sanchitbhadgal36@gmail.com>
2022-01-27 20:04:55 +0530

    learned about importing dependences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:myapp/pages/home_page.dart';
 import 'package:myapp/pages/login_page.dart';
@@ -14,11 +15,12 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
         '/': (context) => LoginPage(),
         '/home': (context) => HomePage(),
