3e89867ecea9085d5116d40b9c5f349274ff274d
Sanchit <sanchitbhadgal36@gmail.com>
2022-01-27 19:26:51 +0530

    learned about routes, theme, and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/home_page.dart';
+
+import 'package:myapp/pages/home_page.dart';
+import 'package:myapp/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
     );
   }
 }
